d63d59f73ea79c3b174c00fec7e7b1f7e45a0ec1
adkar1328 <adkar1328@oslomet.no>
2026-02-27 16:22:10 +0100

    ferdig nå, fikses små feil.

--- init-scripts/01-init-database.sql
+++ init-scripts/01-init-database.sql
@@ -33,10 +33,35 @@ CREATE TABLE leie (
 
 CREATE TABLE betaling (
                           betaling_id SERIAL PRIMARY KEY,
-                          belop INT CHECK (belop >= 0),
+                          belop decimal CHECK (belop >= 0),
                           betalingstidspunkt TIMESTAMP NOT NULL,
                           betalingsmetode VARCHAR(50),
                           leie_id INT REFERENCES leie(leie_id)
 );
 
 SELECT 'Database initialisert!' as status;
+-- Rolle
+CREATE ROLE kunde;
+
+-- Bruker
+CREATE USER kunde_1 WITH PASSWORD 'kunde123';
+
+-- Knytt bruker til rolle
+GRANT kunde TO kunde_1;
+
+-- Gi lese-tilgang til tabeller
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO kunde;
+
+-- Gi tilgang til fremtidige tabeller
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT SELECT ON TABLES TO kunde;
+
+CREATE VIEW kunde_oversikt AS
+SELECT bruker_id, navn, epost
+FROM bruker;
+
+GRANT CONNECT ON DATABASE oblig01 TO kunde;
+GRANT USAGE ON SCHEMA public TO kunde;
+GRANT SELECT ON kunde_oversikt TO kunde;
+
+
